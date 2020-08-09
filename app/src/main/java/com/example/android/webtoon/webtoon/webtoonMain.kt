bfbef7f8257249e126b50d74ab297cd2b8a9f16a
hello2727 <jihye7369@gmail.com>
2020-08-10 04:01:24 +0900

    요일별 웬툰 카테고리 설정 -> TabLayout

--- app/src/main/java/com/example/android/webtoon/webtoon/webtoonMain.kt
+++ app/src/main/java/com/example/android/webtoon/webtoon/webtoonMain.kt
@@ -1,15 +1,12 @@
 package com.example.android.webtoon.webtoon
 
-import android.content.Intent
 import android.os.Bundle
 import android.util.Log
-import android.view.Gravity.apply
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.GravityCompat.apply
+import androidx.fragment.app.FragmentActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
@@ -19,12 +16,14 @@ import androidx.viewpager2.widget.ViewPager2
 import com.example.android.webtoon.R
 import com.example.android.webtoon.webtoon.Interface.Interaction
 import com.example.android.webtoon.webtoon.adapter.ViewPagerAdapter
+import com.example.android.webtoon.webtoon.categorization.Monday
 import com.example.android.webtoon.webtoon.data.RecommendedItem
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 import kotlinx.android.synthetic.main.fragment_webtoon_main.*
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import org.jsoup.Jsoup
-import org.jsoup.nodes.Document
 import org.jsoup.select.Elements
 import java.lang.Exception
 
@@ -40,6 +39,11 @@ class webtoonMain : Fragment(), View.OnClickListener, Interaction {
     private var webtoonUrl = "https://comic.naver.com/index.nhn";
     private lateinit var elements: Elements
     private var imgPath = arrayOfNulls<String>(10)
+
+    private lateinit var tab_week: TabLayout
+    private lateinit var vp_webtoonOfWeek: ViewPager2
+    private lateinit var ViewPagerAdapterOfList: ViewPagerAdapter2
+    private var tab_weekTextArray = arrayOf("신작","월","화","수","목","금","토","일","완결")
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -80,6 +84,13 @@ class webtoonMain : Fragment(), View.OnClickListener, Interaction {
         subscribeObservers()
         autoScrollViewPager()
 
+        tab_week = rootView.findViewById(R.id.tab_week)
+        ViewPagerAdapterOfList = ViewPagerAdapter2(this)
+        vp_webtoonOfWeek = rootView.findViewById(R.id.vp_webtoonOfWeek)
+        vp_webtoonOfWeek.adapter = ViewPagerAdapterOfList
+
+        initViewPagerOfList()
+
         return rootView
     }
 
@@ -116,6 +127,7 @@ class webtoonMain : Fragment(), View.OnClickListener, Interaction {
         }
     }
 
+    /*추천웹툰 아이템*/
     private fun initViewPager2() {
         vp_recommendedWebtoon.apply {
             viewPagerAdapter = ViewPagerAdapter(this@webtoonMain)
@@ -174,5 +186,24 @@ class webtoonMain : Fragment(), View.OnClickListener, Interaction {
 
     override fun onClick(p0: View?) {
 
+    }
+
+    /*항목별 웹툰 리스트*/
+    private fun initViewPagerOfList() {
+        TabLayoutMediator(tab_week,vp_webtoonOfWeek){tab,position->
+            tab.text = tab_weekTextArray[position]
+        }.attach()
+    }
+}
+
+class ViewPagerAdapter2(fa: Fragment):FragmentStateAdapter(fa){
+
+    override fun getItemCount(): Int = 9
+
+    override fun createFragment(position: Int): Fragment {
+        return when(position){
+            1 -> Monday()
+            else -> Monday()
+        }
     }
 }
