d36aa28dc034811911325943f1ffa4bce8b87824
hello2727 <jihye7369@gmail.com>
2021-03-02 01:41:46 +0900

    요일별 웹툰 목록리스트 ui코드 리펙토링

--- app/src/main/java/com/example/android/webtoon/view/initScreenFragment.kt
+++ app/src/main/java/com/example/android/webtoon/view/initScreenFragment.kt
@@ -4,13 +4,11 @@ import android.content.Context
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
-import android.os.Message
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TabHost
 import androidx.activity.OnBackPressedCallback
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
--- app/src/main/java/com/example/android/webtoon/view/view_categorization/Monday.kt
+++ app/src/main/java/com/example/android/webtoon/view/view_categorization/Monday.kt
@@ -2,6 +2,7 @@ package com.example.android.webtoon.view.view_categorization
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -13,14 +14,13 @@ import com.example.android.webtoon.R
 import com.example.android.webtoon.view.adapter.CategoryAdapter
 import com.example.android.webtoon.model.ListItem
 import com.example.android.webtoon.view.deepWebtoonActivity
-import org.jetbrains.anko.doAsync
-import org.jsoup.Jsoup
-import org.jsoup.select.Elements
+import java.text.SimpleDateFormat
+import java.util.*
 
 class Monday : Fragment() {
     private lateinit var rv_listOfWebtoon : RecyclerView
     private lateinit var rvManager: RecyclerView.LayoutManager
-    var webtoonList = arrayListOf<ListItem>(
+    var webtoonList = arrayListOf(
         ListItem("", "소녀의 세계", "9.97", "연재", "모랑지"),
         ListItem("", "유일무이 로맨스", "9.98", "연재", "두부"),
         ListItem("", "인생존망", "9.82", "연재", "박태준/전선욱"),
@@ -32,6 +32,26 @@ class Monday : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         var rootView = inflater.inflate(R.layout.fragment_monday, container, false)
 
+        // 초기화
+        rv_listOfWebtoon = rootView.findViewById(R.id.rv_listOfWebtoon)
+
+        // 요일별로 나누어 웹툰 목록 가져오기
+        getListOfWebtoonsByDay()
+        // 가져온 웹툰 목록 UI에 세팅하기
+        setListOfWebtoonsByDay()
+
+        return rootView
+    }
+
+    private fun getListOfWebtoonsByDay(){
+        //실시간 요일 가져오기
+        var currentTime : Date = Calendar.getInstance().time
+        var weekDay : String = SimpleDateFormat("EE", Locale.getDefault()).format(currentTime)
+
+        Log.d("오늘의 요일은?: ", weekDay)
+    }
+
+    private fun setListOfWebtoonsByDay(){
         val rvAdapter = CategoryAdapter(context, webtoonList) {ListItem ->
             /* 아이템 클릭하면 웹툰 회차 나옴 */
             activity?.let {
@@ -42,7 +62,8 @@ class Monday : Fragment() {
         }
         rvManager = GridLayoutManager(context, 3)
 
-        rv_listOfWebtoon = rootView.findViewById<RecyclerView>(R.id.rv_listOfWebtoon).apply {
+        rv_listOfWebtoon.apply {
+            // apply 의 블록에서는 오직 프로퍼티 만 사용합니다!
             // use this setting to improve performance if you know that changes
             // in content do not change the layout size of the RecyclerView
             setHasFixedSize(true)
@@ -55,20 +76,6 @@ class Monday : Fragment() {
 
             addItemDecoration(DividerItemDecoration(context, GridLayoutManager.VERTICAL))
             addItemDecoration(DividerItemDecoration(context, GridLayoutManager.HORIZONTAL))
-        }
-
-        getSource()
-        return rootView
-    }
-
-    fun getSource(){
-        doAsync {
-            val document = Jsoup.connect("https://comic.naver.com/webtoon/weekday.nhn").get()
-
-            var days : Elements = document.select("div.col_inner h4 span")
-            for(e in days){
-                var day = e.toString().substring(6, 7)
-            }
         }
     }
 }
